eabb1c44365407112f593f1c2b44cfbc1d6dde5d
Minaev Mike <minaev.mike@gmail.com>
2014-11-15 15:29:48 +0300

    Finished

--- pop3.c
+++ pop3.c
@@ -8,16 +8,14 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <string.h>
+#include <sys/ioctl.h>
 
 #define PORT 110
-#define SERVER "pop.qip.ru"
-#define USER ""
-#define PASS ""
 
-int get_ip_by_hostname(char *ip) {
+int get_ip_by_hostname(char *ip, char *server) {
 	struct hostent *he;
 	struct in_addr **addr_list;
-	if ((he = gethostbyname(SERVER)) == NULL) {
+	if ((he = gethostbyname(server)) == NULL) {
 		perror("GETHOSTBYNAME");
 		return 1;
 	}
@@ -32,17 +30,53 @@ int get_ip_by_hostname(char *ip) {
 		return 0;
 	}
 }
+void handle_socket(int sockfd) {
+    int len;
+    int n;
+    char buf[1024];
+    n = recv(sockfd, buf, sizeof(buf) - 1, 0);
+    while (n > 0) {
+        buf[n] = '\0';
+        printf("%s", buf);
+        if (strstr(buf, "\r\n.\r\n") != NULL) {
+            break;
+        }
+        n = recv(sockfd, buf, sizeof(buf) - 1, 0);
+    }
+    printf("Exiting\n");
+}
+void print_list(int sockfd) {
+    write(sockfd, "LIST\n", 5);
+}
 
+void disconnect(int sockfd) {
+    write(sockfd, "QUIT\n", 5);
+}
+
+void print_mail(int sockfd, char *num) {
+    write(sockfd, "RETR ", 5);
+    write(sockfd, num, strlen(num) - 1);
+    write(sockfd, "\n", 1);
+}
+
+void print_help(char *msg) {
+    printf("%s\n", msg);
+    printf("./pop3c <username> <password> <server>\n");
+    exit(0);
+}
 
 int main(int argc, char **argv) {
+    if (argc != 4) 
+        print_help("Wrong number of arguments");
 	struct sockaddr_in serv_addr;
 	int sockfd = 0;
 	char buf[1024];
+    char cmd[1024];
 	char ip[32];
-    int n;
-	if (get_ip_by_hostname(ip) != 0) {
+    int n, k;
+	if (get_ip_by_hostname(ip, argv[3]) != 0) {
 		perror("Error by getting ip");
-		return -1;
+        print_help("Bad server");
 	}
 	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
 		perror("Can't create socket");
@@ -53,6 +87,7 @@ int main(int argc, char **argv) {
 	serv_addr.sin_port = htons(PORT);
 	if(inet_pton(AF_INET, ip, &serv_addr.sin_addr) <= 0) {
 		perror("Inet_Pton error");
+        return -1;
 	}
 
 	if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
@@ -60,15 +95,52 @@ int main(int argc, char **argv) {
 		return -1;
 	}
     //wrte(sockfd, "\n", 1);
-    write(sockfd, "USER ", 5);
-    write(sockfd, USER, strlen(USER));
-    write(sockfd, "\n", 1);
     int state = 0;
+    char *token;
     while ((n = read(sockfd, buf, sizeof(buf))) > 0) {
-        if (!strncmp(buf, "OK", sizeof(buf)))
+        if (strstr(buf, "+OK") != NULL) {
                 state++;
+            
+        } else{
+            if (state == 1) {
+                print_help("Bad username or password");
+            }
+            if (state == 2) {
+                print_help("Bad username or password");
+            }
+        }
+        if (state == 1) {
+            write(sockfd, "USER ", 5);
+            write(sockfd, argv[1], strlen(argv[1]));
+            write(sockfd, "\n", 1);
+            //Write password
+            continue;
+        }
+        if (state == 2){
+            write(sockfd, "PASS ",5);
+            write(sockfd, argv[2], strlen(argv[2]));
+            write(sockfd, "\n", 1);
+            continue;
+        }
+        if (state == 3)
+            break;
+    }
+    
+    printf("Success login\n\t<list> - to print list\n\t<msg> <n> - get <n>th message\n\t<exit> - exit\n");
+    while ((k = read(0, cmd, sizeof(cmd))) > 0) {
+        if (strstr(cmd, "list") != NULL) {
+            print_list(sockfd);
+            handle_socket(sockfd);
+        } else if (strstr(cmd, "msg") != NULL) {
+            token = strtok(cmd, " \t");
+            char* msg = strtok(NULL," \t");
+            print_mail(sockfd, msg);
+            handle_socket(sockfd);
+        } else if (strstr(cmd, "exit") != NULL) {
+            disconnect(sockfd);
+            break;
+        }
 
-        printf("%s!!\n",buf;
     }
     close(sockfd);
 }
